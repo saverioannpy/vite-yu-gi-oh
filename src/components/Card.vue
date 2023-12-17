ab26896b1a57678b78d32ad9959bd5d4bf378b7e
saverioannpy <saverio.annunziata@outlook.com>
2023-12-17 14:23:37 +0100

    feat: grid layout fix

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,19 +6,29 @@ export default {
 </script>
 
 <template>
-  <div class="card-container">
-    <div class="card-img">
-      <img :src="img" alt="name">
+    <div class="card-container d-flex flex-column align-items-center">
+      <div class="card-img">
+        <img :src="img" alt="name" class="img-fluid">
+      </div>
+      <div class="card-name">
+        <p>{{  name }}</p>
+      </div>
+      <div class="card-archetype">
+        <p> {{  archetype }}</p>
+      </div>
     </div>
-    <div class="card-name">
-      <p>{{  name }}</p>
-    </div>
-    <div class="card-archetype">
-      <p> {{  archetype }}</p>
-    </div>
-  </div>
+
 </template>
 
-<style>
+<style lang="scss">
+
+  .card-container{
+    width: calc(100% / 5 - 25px);
+    margin-right: 22px;
+    margin-bottom: 15px;
+    background-color: #d48f38;
+  };
+
+  
 
 </style>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -24,7 +24,7 @@ export default {
 <template>
   <div class="container">
     <div class="cards-container d-flex flex-wrap">
-      <Card v-for="card in cards" :name="card.name" :archetype="card.archetype" :img="card.card_images[0].image_url_small"/>
+      <Card v-for="card in cards" :name="card.name" :archetype="card.archetype" :img="card.card_images[0].image_url"/>
     </div>
   </div>
 </template>
@@ -33,6 +33,12 @@ export default {
 
   .cards-container{
     background-color: #ffffff;
-  }
+    padding: 45px;
+
+    :nth-child(5n + 5){
+      margin-right: 0px;
+    }
+
+  };
 
 </style>
